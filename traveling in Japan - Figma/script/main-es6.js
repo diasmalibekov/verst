da781e43d81faada9e7ad9ee9ef9e250eba97d79
diasmalibekov <67695163+diasmalibekov@users.noreply.github.com>
2020-12-21 10:09:12 +0500

    traveling in Japan finished

--- traveling in Japan - Figma/script/main-es6.js
+++ traveling in Japan - Figma/script/main-es6.js
@@ -14,12 +14,20 @@
     let burgerItem = document.querySelector('.burger');
     let menu = document.querySelector('.header__nav');
     let menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
     });
     menuCloseItem.addEventListener('click', () => {
          menu.classList.remove('header__nav_active');
     });
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav_active');    
+                });
+        }
+    }
 }());
 
 (function () {
