019a947519e52b5505449cd954aa2b71d1c89443
Dias Malibekov <pika4u7@gmail.com>
2021-01-24 09:02:48 +0500

    vue basics

--- vue/app.js
+++ vue/app.js
@@ -7,18 +7,34 @@ const App = {
             notes: ['note 1', 'note 2']
         }
     },
-    methods: {
-        inputChangeHandler(event) {
-            this.inputValue = event.target.value
-        },
+
+    methods:  {
         addNewNote() {
             if (this.inputValue !== '') {
                 this.notes.push(this.inputValue)
                 this.inputValue = ''
             }
         },
+        noteToUpperCase(item) {
+            return item.toUpperCase()
+        },
         deleteNote(idx, event) {
             this.notes.splice(idx, 1)
+        }
+    },
+
+    computed: {
+    doubleCountComputed() {
+            console.log('doubleCount method')
+            return this.notes.length * 2
+        }
+    },
+
+    watch: {
+        inputValue(value) {
+            if(value.length > 10) {
+                this.inputValue = ''
+            }
         }
     }
 }
